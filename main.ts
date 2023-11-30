47bdb6f32e6fe03b79888900946992ed1fcd3ed5
gamermaker27 <149530359+gamermaker27@users.noreply.github.com>
2023-11-30 13:41:51 -0600

    some small things

--- main.ts
+++ main.ts
@@ -287,11 +287,21 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestClosed, function (sp
         bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
         bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
         `)
-    for (let index = 0; index < 6; index++) {
+    for (let index = 0; index < 8; index++) {
         spawning()
     }
     health()
     tiles.placeOnRandomTile(mySprite, sprites.castle.rock0)
+})
+controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
+    music.play(music.melodyPlayable(music.buzzer), music.PlaybackMode.InBackground)
+    game.showLongText("use arrow keys to move and look around. press \"A\" to jump. press \"B\"to shoot the Ghouls. find and Shoot all Ghouls and find all the chests to win", DialogLayout.Full)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    music.play(music.melodyPlayable(music.powerUp), music.PlaybackMode.InBackground)
+    game.showLongText("here have an extra life", DialogLayout.Bottom)
+    sprites.destroy(otherSprite, effects.hearts, 2000)
+    info.changeLifeBy(1)
 })
 function health () {
     health2 = sprites.create(img`
@@ -317,7 +327,7 @@ function health () {
         ....................
         `, SpriteKind.Food)
     tiles.placeOnRandomTile(health2, assets.tile`myTile`)
-    health2.setScale(1.5, ScaleAnchor.Middle)
+    health2.setScale(1.2, ScaleAnchor.Middle)
     animation.runImageAnimation(
     health2,
     [img`
@@ -472,16 +482,6 @@ function health () {
     true
     )
 }
-controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
-    music.play(music.melodyPlayable(music.buzzer), music.PlaybackMode.InBackground)
-    game.showLongText("use arrow keys to move and look around. press \"A\" to jump. press \"B\"to shoot the Ghouls. find and Shoot all Ghouls and find all the chests to win", DialogLayout.Full)
-})
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
-    music.play(music.melodyPlayable(music.powerUp), music.PlaybackMode.InBackground)
-    game.showLongText("here have an extra life", DialogLayout.Bottom)
-    sprites.destroy(otherSprite, effects.hearts, 2000)
-    info.changeLifeBy(1)
-})
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestOpen, function (sprite, location) {
     game.showLongText("Press \"A\" to continue ", DialogLayout.Full)
     tiles.setCurrentTilemap(tilemap`level5`)
@@ -693,7 +693,7 @@ game.setDialogFrame(img`
 game.showLongText("You find yourself in the dreaded Maze of Shadows, surrounded by malevolent ghouls guarding three hidden chests. ", DialogLayout.Full)
 game.showLongText("You must navigate the ghoulish maze and evade the sinister spirits to claim the treasure or face a haunting demise.", DialogLayout.Full)
 game.showLongText("use arrow keys to move and look around. press \"A\" to jump. press \"B\"to shoot the Ghouls. find and Shoot all Ghouls and find all the chests to win", DialogLayout.Full)
-music.play(music.createSong(hex`0064000408020107001c00020a006400f4016400000400000000000000000000000000000000036b000000040002081d0400080002162008000c000208240c001000020f271000140001271400180002142418001c0001081c0020000118200024000211272400280002182728002c0001242c00300001123000340001083400380002182738003c0003080f273c004000021224`), music.PlaybackMode.LoopingInBackground)
+music.play(music.createSong(hex`0064000408020500001c00010a006400f401640000040000000000000000000000000005000004060000000800011103001c0001dc00690000045e0100040000000000000000000005640001040003060000004000010507001c00020a006400f4016400000400000000000000000000000000000000036d000000040002081d0400080002162008000c000208240c001000020f271000140001271400180002142418001c0001081c0020000118200024000211272400280002182728002c0001242c0030000112300034000208183400380002182738003c0004080f16273c00400002122408001c000e050046006603320000040a002d000000640014000132000201000212000c001000011414002400011924002800011809010e02026400000403780000040a000301000000640001c80000040100000000640001640000040100000000fa0004af00000401c80000040a00019600000414000501006400140005010000002c0104dc00000401fa0000040a0001c8000004140005d0076400140005d0070000c800029001f40105c201f4010a0005900114001400039001000005c201f4010500058403050032000584030000fa00049001000005c201f4010500058403c80032000584030500640005840300009001049001000005c201f4010500058403c80064000584030500c8000584030000f40105ac0d000404a00f00000a0004ac0d2003010004a00f0000280004ac0d9001010004a00f0000280002d00700040408070f0064000408070000c80003c800c8000e7d00c80019000e64000f0032000e78000000fa00032c01c8000ee100c80019000ec8000f0032000edc000000fa0003f401c8000ea901c80019000e90010f0032000ea4010000fa0001c8000004014b000000c800012c01000401c8000000c8000190010004012c010000c80002c800000404c8000f0064000496000000c80002c2010004045e010f006400042c010000640002c409000404c4096400960004f6090000f40102b80b000404b80b64002c0104f40b0000f401022003000004200300040a000420030000ea01029001000004900100040a000490010000900102d007000410d0076400960010d0070000c800120010001100010f20002100010f2c002d00010f`), music.PlaybackMode.LoopingInBackground)
 Render.setViewMode(ViewMode.raycastingView)
 mySprite = Render.getRenderSpriteVariable()
 scene.setBackgroundImage(img`
@@ -826,6 +826,6 @@ game.setGameOverMessage(true, "you win!!")
 game.setGameOverEffect(false, effects.clouds)
 game.setGameOverMessage(false, "don't touch the Ghouls  ")
 health()
-for (let index = 0; index < 4; index++) {
+for (let index = 0; index < 6; index++) {
     spawning()
 }
